a4a809ee4b33bda0b45415e2a3d6eddc46a5686d
JayPi4c <jonas.jp4c@gmail.com>
2019-01-05 16:20:04 +0100

    Erweiterung um Vectorklasse und Gauß-Algorithmus
    Mit diesem Commit wird eine Vector-Klasse hinzugefügt und der
    Gaußalgorithmus wurder der Matrixklasse hinzugefügt, da es sehr viel
    schneller gehr als mit der Cramer'schen Regel.

--- src/com/JayPi4c/Matrix.java
+++ src/com/JayPi4c/Matrix.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ThreadLocalRandom;
 /**
  * 
  * @author JayPi4c
- * @version 1.0.1
+ * @version 1.1.0
  */
 public class Matrix implements Serializable {
 
@@ -464,6 +464,168 @@ public class Matrix implements Serializable {
 			throw new IllegalArgumentException("The data does not fit in the row!");
 		this.data[row] = data;
 		return this;
+	}
+
+	/**
+	 * Dieser Code stammt von: <a href=
+	 * "https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Gau%C3%9F-Algorithmus/Musterloesung">https://wiki.freitagsrunde.org</a>
+	 * 
+	 * @since 1.1.0
+	 * @param m
+	 * @param v
+	 * @return
+	 */
+	public static Vector getSolution(Matrix m, Vector v) {
+		Vector vector = v.copy();
+		// Das Gleichungssystem hat keine eindeutige Loesung!
+		if (m.data.length < m.data[0].length)
+			throw new IllegalArgumentException("Gleichungssystem nicht eindeutig loesbar!");
+
+		// Merken der Spalte, welche eine Zahl ungleich null besitzt
+		int tmpColumn = -1;
+
+		// Alle Zeilen durchgehen: Ziel der for-Schleife -> Matrix in
+		// Zeilenstufenform bringen!
+		// -> Alle Zahlen unterhalb der Diagonale sind null
+		for (int line = 0; line < m.data.length; line++) {
+			tmpColumn = -1;
+
+			// Umformungsschritt 1: Finden einer Spalte mit einem Wert ungleich
+			// null
+			for (int column = 0; column < m.data[line].length; column++) {
+				for (int row = line; row < m.data.length; row++) {
+					if (m.data[row][column] != 0) {
+						tmpColumn = column;
+						break;
+					}
+				}
+
+				// Abbruch, zahl ungleich null wurde gefunden
+				if (tmpColumn != -1) {
+					break;
+				}
+			}
+
+			// NullZeile(n) entdeckt!
+			if (tmpColumn == -1) {
+				for (int row = line; row < m.data.length; row++) {
+					// Gleichungssystem hat keine Loesung!
+					if (vector.data[line] != 0)
+						throw new IllegalArgumentException("Gleichungssystem besitzt keine Loesung!");
+				}
+				// Nullzeile(n) vorhanden -> Ist das System noch eindeutig
+				// loesbar?
+				if (m.data[0].length - 1 >= line) {
+					// System nicht eindeutig loesbar.
+					throw new IllegalArgumentException("Gleichungssystem nicht eindeutig loesbar!");
+				}
+				break;
+			}
+
+			// Umformungsschritt 2: Die Zahl matrix[line][tmpColumn] soll
+			// UNgleich null sein
+			if (m.data[line][tmpColumn] == 0) {
+				for (int row = line + 1; row < m.data.length; row++) {
+					if (m.data[row][tmpColumn] != 0) {
+
+						// Vertauschen von Zeilen -> matrix[line][tmpColumn]
+						// wird dann ungleich null
+						swapTwoLines(line, row, m.data, vector);
+						break;
+					}
+				}
+			}
+
+			// Umformungsschritt 3: matrix[line][tmpColumn] soll gleich 1 sein.
+			if (m.data[line][tmpColumn] != 0) {
+
+				// Division der Zeile mit matrix[line][tmpColumn]
+				divideLine(line, m.data[line][tmpColumn], m.data, vector);
+			}
+
+			// Umformungsschritt 4: Alle Zahlen unter matrix[line][tmpColumn]
+			// sollen null sein.
+			for (int row = line + 1; row < m.data.length; row++) {
+
+				// Subtraktion damit unter der Zahl im Umformungsschritt 3 nur
+				// nullen stehen
+				removeRowLeadingNumber(m.data[row][tmpColumn], line, row, m.data, vector);
+			}
+		}
+
+		// Umformungsschritt 6: Matrix in Normalform bringen (Zahlen oberhalb
+		// der Diagonale werden ebenfalls zu null)
+		for (int column = m.data[0].length - 1; column > 0; column--) {
+
+			// Alle Werte oberhalb von "column" werden zu null
+			for (int row = column; row > 0; row--) {
+				// Dazu wird Subtraktion angewandt
+				removeRowLeadingNumber(m.data[row - 1][column], column, row - 1, m.data, vector);
+			}
+		}
+
+		// Unser ehemaliger Loesungsvektor ist jetzt zu unserem Zielvektor
+		// geworden :)
+		return vector;
+	}
+
+	/**
+	 * Dieser Code stammt von: <a href=
+	 * "https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Gau%C3%9F-Algorithmus/Musterloesung">https://wiki.freitagsrunde.org</a>
+	 * 
+	 * @since 1.1.0
+	 * @param rowOne
+	 * @param rowTwo
+	 * @param matrix
+	 * @param vector
+	 */
+	private static void swapTwoLines(int rowOne, int rowTwo, double[][] matrix, Vector vector) {
+		double[] tmpLine;
+		double tmpVar;
+
+		tmpLine = matrix[rowOne];
+		tmpVar = vector.data[rowOne];
+
+		matrix[rowOne] = matrix[rowTwo];
+		vector.data[rowOne] = vector.data[rowTwo];
+
+		matrix[rowTwo] = tmpLine;
+		vector.data[rowTwo] = tmpVar;
+	}
+
+	/**
+	 * Dieser Code stammt von: <a href=
+	 * "https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Gau%C3%9F-Algorithmus/Musterloesung">https://wiki.freitagsrunde.org</a>
+	 * 
+	 * @since 1.1.0
+	 * @param row
+	 * @param div
+	 * @param matrix
+	 * @param vector
+	 */
+	private static void divideLine(int row, double div, double[][] matrix, Vector vector) {
+		for (int column = 0; column < matrix[row].length; column++) {
+			matrix[row][column] = matrix[row][column] / div;
+		}
+		vector.data[row] = vector.data[row] / div;
+	}
+
+	/**
+	 * Dieser Code stammt von: <a href=
+	 * "https://wiki.freitagsrunde.org/Javakurs/%C3%9Cbungsaufgaben/Gau%C3%9F-Algorithmus/Musterloesung">https://wiki.freitagsrunde.org</a>
+	 * 
+	 * @since 1.1.0
+	 * @param factor
+	 * @param rowRoot
+	 * @param row
+	 * @param matrix
+	 * @param vector
+	 */
+	private static void removeRowLeadingNumber(double factor, int rowRoot, int row, double[][] matrix, Vector vector) {
+		for (int column = 0; column < matrix[row].length; column++) {
+			matrix[row][column] = matrix[row][column] - factor * matrix[rowRoot][column];
+		}
+		vector.data[row] = vector.data[row] - factor * vector.data[rowRoot];
 	}
 
 	/**
